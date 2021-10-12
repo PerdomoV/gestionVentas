35f78e5870916d25fc00c6e19d6060b75587696a
PerdomoV <jorgamow@gmail.com>
2021-10-11 20:54:31 -0500

    borrado de usuarios implementado con éxito

--- src/Controllers/usuarioController.js
+++ src/Controllers/usuarioController.js
@@ -43,6 +43,39 @@ module.exports = {
         }
     },
     actualizar: async (req, res) => {
-        
+
+        try{ 
+            let {id, nombre, cedula, email, password, telefono, rol} = req.body
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            password = hash
+
+            const filtro = {_id: id}
+            const actualizacion = {nombre, cedula, email, password, telefono, rol}
+            const usuarioActualizado = await Usuario.findOneAndUpdate(filtro, actualizacion);
+            
+            console.log(usuarioActualizado)
+            
+            if(!usuarioActualizado) return res.status(500).json({ mensaje: "No se pudo actualizar el usuario" })
+            
+            return res.status(201).json({message:'Usuario actualizado con éxito'})            
+        }catch(e){
+            console.error(e)
+            return res.status(500).json({ message: e })    
+        }
     },
+
+    borrar: async (req, res) => {
+        try {
+            let { id } = req.body 
+            filtro = {_id : id}
+            let borrado = await Usuario.deleteOne(filtro)
+            console.log(borrado)
+            if(!borrado) return res.status(500).json({mensaje: "Error al borrar"})
+            return res.status(200).json({mensaje: "Elemento borrado con exito"})
+        }catch(e){
+            console.error(e)
+            return res.status(500).json({ message: e }) 
+        }
+    }
 }
--- src/routes/rutasUsuario.js
+++ src/routes/rutasUsuario.js
@@ -4,5 +4,9 @@ const usuarioController = require("../Controllers/usuarioController.js")
 
 const router = Router()
 router.post('/usuario/crear', usuarioController.crear)
+router.post('/usuario/actualizar', usuarioController.actualizar)
+router.put('/usuario/borrar', usuarioController.borrar)
+
+
 
 module.exports = router
